ee2a69e5303cb13ee4b67d57e259800a178de6fd
mohammed-ridwan-tn <160103503+mohammed-ridwan-tn@users.noreply.github.com>
2024-07-18 11:13:45 +0530

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
